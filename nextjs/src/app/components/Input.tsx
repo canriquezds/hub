fb7ee32a03d4a05a502a8b4602d0cfb3b5beb114
Todd Runstein <toddrun@gmail.com>
2024-05-31 14:08:01 -0700

    Basic document summaries

--- nextjs/src/app/components/Input.tsx
+++ nextjs/src/app/components/Input.tsx
@@ -2,14 +2,52 @@
 'use client';
 
 import { useSession } from 'next-auth/react'
-import { HiOutlinePhotograph } from 'react-icons/hi';
 
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Input() {
   const {data: session } = useSession();
+  const [content, setContent] = useState("");
+  const [summary, setSummary] = useState("");
+
+  const fulcrumDomain = process.env.NEXT_PUBLIC_FULCRUM_DOMAIN;
+  const apiToken = process.env.NEXT_PUBLIC_FULCRUM_API_TOKEN || 'will_not_work';
 
   if (!session) return null;
+
+  const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.target.value);
+  }
+
+  const handleSubmit = () => {
+    setSummary('...');
+    const prompt = `You are a expert copy writer on technical sw development blogposts.
+      Produce a summary of this blogpost in no more than 2 sentences. Make it serious and informative to make it easy
+      for readers to understand the content: 
+      """ ${content} """`;
+
+    fetch(`https://${fulcrumDomain}/api/v2/llm/generate`, {
+      method: "POST",
+      headers: {
+        "X-ApiToken": apiToken,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        template: prompt
+      })
+    })
+      .then((res) => {
+        console.log('RES', res)
+        if (!res.ok) {
+          setSummary('Failed to fetch')
+        }
+        return res.json()})
+      .then((data) => {
+        const response = data.candidates[0].content.parts[0].text;
+        setSummary(response);
+      })
+  }
 
   return (
     <div className='flex border-b border-gray-200 p-3 space-x-3 w-full'>
@@ -17,15 +55,19 @@ export default function Input() {
       <div className='w-full divide-y divide-gray-200 '>
         <textarea
           className='w-full border-none outline-none tracking-wide min-h-[50px] text-gray-700'
-          placeholder='Whats happening' rows={2}
+          placeholder='Paste your documentation here' rows={8}
+          onChange={handleContentChange}
         ></textarea>
         <div className='flex items-center justify-between pt-2.5'>
-          <HiOutlinePhotograph className='h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer' />
           <button
-            disabled
+            onClick={ handleSubmit }
+            disabled={!content}
             className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brigthness-95 disabled:opacity-50'>
             Post
           </button>
+        </div>
+        <div>
+          {summary}
         </div>
       </div>
     </div>
